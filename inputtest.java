3e9f5d1a03da7f45b874ddce8f3a15f735343b1f
yuye <yeyu456@gmail.com>
2014-05-19 10:23:38 +0800

    java input

---
+++ inputtest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class inputtest {
+
+	public static void main(String[] args) {
+		Scanner test_input = new Scanner(System.in);
+		double one = test_input.nextDouble();
+		double two = test_input.nextDouble();
+		final int j = 1;
+		System.out.println(one);
+		System.out.println(two);
+		System.out.println(j);
+	}
+
+}
