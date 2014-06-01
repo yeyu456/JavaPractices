c8e9fe0a43aaec433bd4b4d9c00eb711e9a52ee5
yuye <yeyu456@gmail.com>
2014-06-01 16:07:24 +0800

    r1.Translate java file to html view

--- JavaToHtml.java
+++ JavaToHtml.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.HashSet;
 import java.io.FileReader;
-import java.io.DataInputStream;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,14 +8,20 @@ import java.io.IOException;
 public class JavaToHtml {
     public static void main(String[] args){
         JTH test = new JTH();
+        int count = 1;
         try(BufferedReader br = new BufferedReader(new FileReader(args[0]))){
             FileWriter fw = new FileWriter(args[1]);
+            fw.write(test.htmlHeader);
+            fw.flush();
             String line = br.readLine();
             while(line!=null){
                 fw.write(test.translate(line));
                 fw.flush();
                 line = br.readLine();
+                count++;
             }
+            fw.write(test.htmlTailer);
+            fw.flush();
             fw.close();
         }catch(IOException e){
             e.printStackTrace();
@@ -25,14 +30,14 @@ public class JavaToHtml {
 }
 
 class JTH {
-    private final String htmlHeader = "<html>\n<head><title>Java File</title></head>\n<body>\n";
-    private final String htmlTailer = "</body>\n</html>";
+    final String htmlHeader = "<html>\n<head><title>Java File</title></head>\n<body>\n";
+    final String htmlTailer = "</body>\n</html>";
     private final String rowHeader = "<p>";
     private final String rowTailer = "</p>\n";
     private final String typeForKeyWord = "<b><font color=\"#000079\">";
     private final String typeForComment = "<b><font color=\"#28FF28\">";
     private final String typeForData = "<b><font color=\"#0066CC\">";
-    private final String typeTailer = "</font></b>\n";
+    private final String typeTailer = "</font></b>";
     
     private final String[] keywords = {"abstract", "boolean", "break",
                                        "byte", "case", "catch", "char",
@@ -45,56 +50,84 @@ class JTH {
                                        "static", "super", "switch", "this", 
                                        "throw", "throws", "try", "void", "while"}; 
     private final HashSet<String> kw = new HashSet<String>(Arrays.asList(keywords));
+    boolean isString = false;
     
     public String translate (String line){
         boolean isComment = false;
-        boolean isString = false;
         String newLine = rowHeader;
         String temp = "";
-        line = line.replaceAll("\t", "\\s{4}");
-        String[] word = line.split("\\s");
+        line = line.replaceAll("\t", "\\s{4}").replaceAll("\\s", "&nbsp;");
+        String[] word = line.split("&nbsp;");
         for(String s:word){
-            if(!isComment&&!isString){
-                if(s=="//"||s.matches("//")){
+            s = s.replace(">", "&gt;");
+            s = s.replace("<", "&lt;");
+            if(!isComment){
+                if(s.contains("//")){
                     isComment = true;
-                    newLine += typeForComment + s;
-                    continue;
+                    temp = s.replaceFirst("//", typeForComment + "//");
                 }
                 else
-                    if(s.matches("\"")){
-                        isString = true;
-                        temp += typeForData + s;
+                    if(s.contains("\"")){
+                        temp = this.stringCheck(s);
                     }
                     else{
                         temp = this.keywordCheck(s);
-                        temp = this.dataCheck(s);
+                        temp = this.dataCheck(temp);
                     }
             }
             else{
-                if(isString&&s.matches("\"")){
-                    isString = false;
-                    temp += s + typeTailer;
-                }
+                if(isComment)
+                    temp = s;
             }
-            newLine += temp;
+            newLine += "&nbsp;" + temp;
         }
-        if(isComment)
-            newLine += typeTailer;
+        if(isComment){
+            newLine += "&nbsp;" + typeTailer;
+            isComment = false;
+        }
+        newLine += rowTailer;
         return newLine;
     }
     
     private String keywordCheck(String s){
-        String tmp = "";
-        for(String n:s.split("[(){}]="))
+        for(String n:s.split("[\\(\\)\\{\\}\\[\\]=<>]")){
             if(kw.contains(n))
-                tmp += typeForKeyWord + n + typeTailer;
-        return tmp;
+                s = s.replace(n, typeForKeyWord + n + typeTailer);
+        }
+        return s;
     }
     
     private String dataCheck(String s){
-        if(s.matches("(=*)(\\d+|null)")){
-            s = s.replaceAll("(=*)(\\d+|null);", 
-                    typeForData + "\1\2" + typeTailer);
+        int index = 0;
+        for(String n:s.split("[\\(\\)\\{\\}\\[\\]=><\\+\\-\\*%\\/;,]")){
+            if(n.matches("\\d+|null")){
+                s = s.substring(0, index) 
+                    + typeForData + n 
+                    + typeTailer 
+                    + s.substring(index+n.length());
+                index += typeForData.length() + typeTailer.length();
+            }
+            index += n.length() + 1;
+        }
+        return s;
+    }
+    
+    public String stringCheck(String s){
+        for(String n:s.split("[\\(\\)\\{\\}\\[\\]=\\+;,]")){
+            if(n.matches("\\\".*\\\""))
+                s = s.replace(n, typeForData + n + typeTailer);
+            else{
+                if(n.contains("\"")){
+                    if(isString){
+                        isString = false;
+                        s = s.replace("\"", "\"" + typeTailer);
+                    }
+                    else{
+                        isString = true;
+                        s = s.replaceFirst("\"", typeForData + "\"");
+                    }
+                }
+            }
         }
         return s;
     }
